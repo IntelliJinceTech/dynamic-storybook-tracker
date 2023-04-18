40acb6620fc1fb460ffaf6d807e504f8037358eb
Christopher Jin <chrisjin1@gmail.com>
2023-04-17 22:02:39 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
+const connectDB = require('./config/db');
+
+//  load config
+dotenv.config({ path: './config/config.env' });
+
+//  passport config
+require('./config/passport')(passport);
+
+connectDB();
+
+const app = express();
+
+//  Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+//  Handlebars
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+//  Session Middleware
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+//  Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//  Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//  ROUTES
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 8888;
+
+app.listen(
+    PORT,
+    console.log(
+        `Server running in to ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
