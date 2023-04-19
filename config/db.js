cdeb17e4d68f506d2acf964aac90b5fb9425a891
Christopher Jin <chrisjin1@gmail.com>
2023-04-19 12:15:12 -0400

    traversy auth route protection

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: 'storybooks',
+        })
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
-        console.error(error);
-        process.exit(1);
+        console.error(error)
+        process.exit(1)
     }
-};
+}
 
-module.exports = connectDB;
+module.exports = connectDB
--- config/passport.js
+++ config/passport.js
@@ -11,14 +11,36 @@ module.exports = function (passport) {
                 callbackURL: '/auth/google/callback',
             },
             async (accessToken, refreshToken, profile, done) => {
-                console.log(profile)
+                const newUser = {
+                    googleId: profile.id,
+                    displayName: profile.displayName,
+                    firstName: profile.name.givenName,
+                    lastName: profile.name.familyName,
+                    image: profile.photos[0].value,
+                }
+                try {
+                    let user = await User.findOne({ googleId: profile.id })
+
+                    if (user) {
+                        done(null, user)
+                    } else {
+                        user = await User.create(newUser)
+                        done(null, user)
+                    }
+                } catch (error) {
+                    console.error(error)
+                }
             }
         )
     )
     passport.serializeUser((user, done) => {
         done(null, user.id)
     })
-    passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) => done(err, user))
+    // passport.deserializeUser((id, done) => {
+    //     User.findById(id, (err, user) => done(err, user))
+    // })
+    passport.deserializeUser(async (id, done) => {
+        const user = await User.findById(id)
+        done(null, user)
     })
 }
